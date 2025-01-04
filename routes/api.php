59c49b128daefcd7c605835345131fcb9d87f525
rhyuzi <azitom2@gmail.com>
2025-01-04 09:03:39 +0700

    api login

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\AuthController;
 
 Route::post('/reports', [ReportController::class, 'store']);
 Route::get('/reports', [ReportController::class, 'index']);
 Route::patch('/reports/{id}', [ReportController::class, 'update']);
-
+Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
